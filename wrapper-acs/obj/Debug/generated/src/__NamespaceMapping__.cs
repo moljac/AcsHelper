8e2f909ccde2b0a3987762146a8d70de4717170e
shuaga <shuaga@microsoft.com>
2023-03-31 11:02:36 +0530

    first commit

---
+++ wrapper-acs/obj/Debug/generated/src/__NamespaceMapping__.cs
@@ -0,0 +1,16 @@
+using System;
+
+[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.example.mylibrary", Managed="Com.Example.Mylibrary")]
+
+delegate void _JniMarshal_PP_V (IntPtr jnienv, IntPtr klass);
+delegate void _JniMarshal_PPL_V (IntPtr jnienv, IntPtr klass, IntPtr p0);
+#if !NET
+namespace System.Runtime.Versioning {
+    [System.Diagnostics.Conditional("NEVER")]
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Enum | AttributeTargets.Event | AttributeTargets.Field | AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Module | AttributeTargets.Property | AttributeTargets.Struct, AllowMultiple = true, Inherited = false)]
+    internal sealed class SupportedOSPlatformAttribute : Attribute {
+        public SupportedOSPlatformAttribute (string platformName) { }
+    }
+}
+#endif
+
