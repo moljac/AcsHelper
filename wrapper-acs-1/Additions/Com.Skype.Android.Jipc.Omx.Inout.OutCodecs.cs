2b74a6e44f1f0e4e4687686533196d0c45622a44
moljac <mcvjetko@holisticware.net>
2023-03-31 15:38:33 +0200

    metadata build green

---
+++ wrapper-acs-1/Additions/Com.Skype.Android.Jipc.Omx.Inout.OutCodecs.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Java.Interop;
+
+namespace Com.Skype.Android.Jipc.Omx.Inout 
+{
+	// Metadata.xml XPath class reference: path="/api/package[@name='com.skype.android.jipc.omx.inout']/class[@name='OutCodecs']"
+	// [global::Android.Runtime.Register ("com/skype/android/jipc/omx/inout/OutCodecs", DoNotGenerateAcw=true)]
+	public partial class OutCodecs //: global::Java.Lang.Object, global::Com.Skype.Android.Jipc.Transactor.IOut 
+    {
+    }
+}
